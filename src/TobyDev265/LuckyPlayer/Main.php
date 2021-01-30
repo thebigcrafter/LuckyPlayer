e914f2be675802de512dae87322f4ca7cc4521ac
TobyDev265 <tobydev265@gmail.com>
2021-01-30 21:03:53 +0700

    Upload files

---
+++ src/TobyDev265/LuckyPlayer/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TobyDev265\LuckyPlayer;
+
+use pocketmine\{Server, Player};
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\{Config, TextFormat as C};
+
+use onebone\economyapi\EconomyAPI;
+
+class Main extends PluginBase implements Listener {
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
+    }
+    public function onJoin(PlayerJoinEvent $e) {
+        $player = $e->getPlayer();
+        $luckyNumberB = $this->data->get("number");
+        $this->data->set("number", $luckyNumberB + 1);
+        $this->data->save();
+        $luckyNumberA = $this->data->get("number");
+        if($luckyNumberA == $this->cfg->get("luckyNumber")) {
+            EconomyAPI::getInstance()->addMoney($player, $this->cfg->get("luckyMoney"));
+            $player->sendMessage($this->cfg->get("message"));
+            $this->data->set("number", 0);
+            $this->data->save();
+        }
+    }
+
+}
