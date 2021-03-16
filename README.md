d4f4a72e68602c83f7ebb7dfab7046c02da376df
TobyDev265 <tobyd265@gmail.com>
2021-03-17 05:22:14 +0700

    Update v1.0.1

--- README.md
+++ README.md
@@ -3,10 +3,21 @@
 ![GitHub](https://img.shields.io/github/license/TobyDev265/LuckyPlayer)
 [![](https://poggit.pmmp.io/shield.state/LuckyPlayer)](https://poggit.pmmp.io/p/LuckyPlayer)
 [![](https://poggit.pmmp.io/shield.dl.total/LuckyPlayer)](https://poggit.pmmp.io/p/LuckyPlayer)
-# LuckyPlayer v1.0.0
+# LuckyPlayer v1.0.1
 **Give money to players when they join the server at lucky time.**  
 For example: You config ```luckyMoney: 100``` and ```luckyNumber: 1000```. If player is the 1000th player join the server, they will have 100$  
 ``NOTE!`` Make sure EconomyAPI is installed.
+# Config
+```yaml
+luckyMoney: 100
+# Players who join the 1000th server will receive $ 100
+luckyNumber: 1000
+
+message: "{PLAYER_NAME} is the {LUCKY_NUMBER}th player so he/she have {MONEY}$ for free!"
+# {PLAYER_NAME} for the name of lucky player
+# {MONEY} for the luckyMoney
+# {LUCKY_NUMBER} fro the luckyNumber
+```
 # Installation
 - Make sure that Sheep is installed
 - Then use ```install LuckyPlayer``` to install plugin
--- src/TobyDev265/LuckyPlayer/Main.php
+++ src/TobyDev265/LuckyPlayer/Main.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * LuckyPlayer v1.0.0
+ * LuckyPlayer v1.0.1
  * Copyright TobyDev265
  * Released under the GNU General Public License v2.0 license
  * https://github.com/TobyDev265/LuckyPlayer/blob/main/LICENSE
@@ -20,6 +20,7 @@ use onebone\economyapi\EconomyAPI;
 class Main extends PluginBase implements Listener {
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
@@ -32,7 +33,10 @@ class Main extends PluginBase implements Listener {
         $luckyNumberA = $this->data->get("number");
         if($luckyNumberA == $this->cfg->get("luckyNumber")) {
             EconomyAPI::getInstance()->addMoney($player, $this->cfg->get("luckyMoney"));
-            $player->sendMessage($this->cfg->get("message"));
+            $luckyMessage_1 = str_replace("{PLAYER_NAME}", $player->getName(), $this->cfg->get("message"));
+            $luckyMessage_2 = str_replace("{MONEY}", $this->cfg->get("luckyMoney"), $luckyMessage_1);
+            $luckyMessage_3 = str_replace("{LUCKY_NUMBER}", $this->cfg->get("luckyNumber"), $luckyMessage_2);
+            $player->sendMessage($luckyMessage_3);
             $this->data->set("number", 0);
             $this->data->save();
         }
