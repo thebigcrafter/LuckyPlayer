cf515d3add5ab6c909d06123a285ab008525c4f6
MintoD <mintothedev@protonmail.com>
2021-05-06 19:13:10 +0700

    Revert "Fix PHPStan"
    This reverts commit 055374ba40395129b384385d5c6d5aa33011ae75.

--- src/TobyDev265/LuckyPlayer/Main.php
+++ src/TobyDev265/LuckyPlayer/Main.php
@@ -19,13 +19,6 @@ use onebone\economyapi\EconomyAPI;
 use JackMD\UpdateNotifier\UpdateNotifier;
 
 class Main extends PluginBase implements Listener {
-
-    /**
-     * @var Config
-     */
-    private $cfg;
-    private $data;
-
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
