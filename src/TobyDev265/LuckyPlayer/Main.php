e866cf98833e28f7b110fb6e69d748214e3a5b14
MintoD <tobythedev7@outlook.com>
2022-03-10 12:56:08 +0700

    Use BedrockEconomy instead of EconomyAPI

--- src/TobyDev265/LuckyPlayer/Main.php
+++ src/TobyDev265/LuckyPlayer/Main.php
@@ -9,31 +9,35 @@
 
 namespace TobyDev265\LuckyPlayer;
 
-use pocketmine\{Server, Player};
-use pocketmine\plugin\PluginBase;
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\utils\{Config, TextFormat as C};
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
-use onebone\economyapi\EconomyAPI;
-use JackMD\UpdateNotifier\UpdateNotifier;
+class Main extends PluginBase implements Listener
+{
 
-class Main extends PluginBase implements Listener {
-    public function onEnable() {
+    private Config $cfg;
+    private Config $data;
+
+    public function onEnable(): void
+    {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML);
-        UpdateNotifier::checkUpdate("LuckyPlayer", "1.0.2");
     }
-    public function onJoin(PlayerJoinEvent $e) {
+
+    public function onJoin(PlayerJoinEvent $e)
+    {
         $player = $e->getPlayer();
         $luckyNumberB = $this->data->get("number");
         $this->data->set("number", $luckyNumberB + 1);
         $luckyNumberA = $this->data->get("number");
-        if($luckyNumberA == $this->cfg->get("luckyNumber")) {
-            EconomyAPI::getInstance()->addMoney($player, $this->cfg->get("luckyMoney"));
+        if ($luckyNumberA == $this->cfg->get("luckyNumber")) {
+            BedrockEconomyAPI::getInstance()->addToPlayerBalance($player->getName(), $this->cfg->get("luckyMoney"));
             $luckyMessage_1 = str_replace("{PLAYER_NAME}", $player->getName(), $this->cfg->get("message"));
             $luckyMessage_2 = str_replace("{MONEY}", $this->cfg->get("luckyMoney"), $luckyMessage_1);
             $luckyMessage_3 = str_replace("{LUCKY_NUMBER}", $this->cfg->get("luckyNumber"), $luckyMessage_2);
@@ -42,5 +46,4 @@ class Main extends PluginBase implements Listener {
         }
         $this->data->save();
     }
-
 }
